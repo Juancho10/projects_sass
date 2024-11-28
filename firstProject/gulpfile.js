4fbd353903be60680d57d9ea60238b665cd52759
Juancho10 <juanitolizarazu18@gmail.com>
2024-11-28 16:47:43 -0500

    se configura la tarea de compilación css, se crean primeros archivos sass

--- firstProject/gulpfile.js
+++ firstProject/gulpfile.js
@@ -1,11 +1,22 @@
+//librerias
+import {src, dest, watch} from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
 //se une el gulp-sass con el sass
 const sass = gulpSass(dartSass);
 
-export function prueba( done){
-    console.log("Estas dentro de gulp");
+export function css( done){
+    //se buscan las rutas de archivos sass y se compilan para guardar en css
+    /**
+     * para mostrar error de compilación se escucha con el on(1,2) el cual tiene dos parametros. 1. evento, 2. funcion
+     *  */    
+    src('src/scss/app.scss')
+        .pipe( sass().on('error', sass.logError) )
+        .pipe( dest('build/css') );
     //Se utiliza para indicar que una tarea termina
     done()
+}
+export function dev(){
+    watch('src/scss/app.scss', css);
 }
